3f386af9fca3708479b28f990819b1badd943666
kanana <chalithbandara99@gmail.com>
2024-07-13 19:49:50 +0530

    typescript

---
+++ src/graphQL/resolver.js
@@ -0,0 +1,14 @@
+import {users,books} from '../../database/mockdb.js';
+
+
+// Resolvers define how to fetch the types defined in your schema.
+// This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+    users: () => users,
+  },
+
+};
+
+export {resolvers}
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,7 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import { typeDefs } from './schema/schema.js';
-import {users,books} from '../database/mockdb.js';
-
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-
-
-
-
-// Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
-const resolvers = {
-  Query: {
-    books: () => books,
-    users: () => users,
-  },
-
-};
+import { typeDefs } from './graphQL/schema.js';
+import { resolvers } from './graphQL/resolver.js';
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
