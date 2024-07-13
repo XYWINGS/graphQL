7482ffd3673896190721030fc7e6695707d40766
kanana <chalithbandara99@gmail.com>
2024-07-13 18:59:50 +0530

    mockdb done

---
+++ database/mockdb.js
@@ -0,0 +1,25 @@
+const books = [
+    {
+      title: 'The Awakening',
+      author: 'Kate Chopin',
+    },
+    {
+      title: 'City of Glass',
+      author: 'Paul Auster',
+    },
+  ];
+  
+  const users = [
+    {
+      name: 'Jon Seena',
+      email: 'jon@gg.com',
+      projects: [{ title: "summer upgrades", year: "2023" }],
+    },
+    {
+      name: 'City Glass',
+      email: 'auster@gg.com',
+      projects: [{ title: "winter upgrades", year: "2024" }],
+    },
+  ];
+  
+  export { users, books }
--- src/index.js
+++ src/index.js
@@ -1,33 +1,10 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import {users,books} from '../database/mockdb.js';
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
-
-const books = [
-    {
-      title: 'The Awakening',
-      author: 'Kate Chopin',
-    },
-    {
-      title: 'City of Glass',
-      author: 'Paul Auster',
-    },
-  ];
-
-  const users = [
-    {
-      name: 'Jon Seena',
-      email: 'jon@gg.com',
-      projects : [{title : "summer upgrades" , year : "2023"}]
-    },
-    {
-      name: 'City Glass',
-      email: 'auster@gg.com',
-      projects : [{title : "winter upgrades" , year : "2024"}]
-    },
-  ];
 
 
 const typeDefs = `#graphql
